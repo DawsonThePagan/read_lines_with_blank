8b2e996f117a8b6cadcc128d5beef48b038ed5a5
Bailey Dawson <dawson270500@gmail.com>
2025-04-19 01:18:03 +0100

    Change to open file rather than need buffer
    Opens the file itself now rather than being passed a buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::env::consts::OS;
 use std::fs::File;
 use std::error::Error;
-use std::io::{self, BufReader, Read};
+use std::io::{self, Read};
 
 const NEW_LINE_WINDOWS: &str = "\r\n";
 const NEW_LINE_LINUX: &str = "\n";
@@ -12,15 +12,12 @@ const NEW_LINE_LINUX: &str = "\n";
 /// ```norun
 /// use read_lines_with_blank::read_lines_with_blank;
 ///
-/// let f = File::open("foo.txt")?;
-/// let mut reader = BufReader::new(f);
-///
-/// let lines = match read_lines_with_blank(&mut reader) {
+/// let lines = match read_lines_with_blank("foo_bar.txt") {
 ///     Ok(x) => x,
 ///     Err(e) => return Err(e),
 /// };
 /// ```
-pub fn read_lines_with_blank(buf_read: &mut BufReader<File>) -> Result<Vec<String>, Box<dyn Error>> {
+pub fn read_lines_with_blank(file: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut ret: Vec<String> = Vec::new();
     let new_line = match OS {
         "linux" => NEW_LINE_LINUX,
@@ -28,8 +25,11 @@ pub fn read_lines_with_blank(buf_read: &mut BufReader<File>) -> Result<Vec<Strin
         _ => return Err(Box::from(io::Error::new(io::ErrorKind::Unsupported, "Unsupported OS"))),
     };
 
+    let f = File::open(file)?;
+    let mut reader = std::io::BufReader::new(f);
+
     let mut data: Vec<u8> = Vec::new();
-    buf_read.read_to_end(&mut data)?;
+    reader.read_to_end(&mut data)?;
     let str: String = match String::from_utf8(data) {
         Ok(s) => s,
         Err(e) => return Err(Box::from(e)),
